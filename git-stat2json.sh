8d6306c2994343c5e9575c32341f29558b0eba0f
textarcana <noah@onemorebug.com>
2013-11-03 15:26:19 -0500

    git --stat in JSON format

---
+++ git-stat2json.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+git log \
+    --numstat \
+    --format='%H' \
+    $@ | \
+    perl -lawne 'BEGIN{};
+        if (defined $F[1]) {
+            print qq#{"insertions": "$F[0]", "deletions": "$F[1]", "path": "$F[2]"},#
+        } elsif (defined $F[0]) {
+            print qq#],\n"$F[0]": [#
+        };
+        END{print qq#],#}' | \
+    tail -n +2 | \
+    perl -wpe 'BEGIN{print "{"}; END{print "}"}' | \
+    tr '\n' ' ' | \
+    perl -wpe 's#(]|}),\s*(]|})#$1$2#g' | \
+    perl -wpe 's#,\s*?}$#}#' | \
+    jsonlint -s
